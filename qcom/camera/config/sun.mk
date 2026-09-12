ff9b14b3a7f59462805dc2352b2fac1f23f63512
grep <grepfox@tutamail.com>
2026-09-12 20:52:04 +0000

    qcom: camera: Import oplus changes for volkswagen

--- qcom/camera/config/sun.mk
+++ qcom/camera/config/sun.mk
@@ -23,3 +23,12 @@ dtbo-$(CONFIG_ARCH_SUN)   += oplus/pagani-camera-overlay-T0.dtbo \
 dtbo-$(CONFIG_ARCH_SUN)   += oplus/paganiIn-camera-overlay-T0.dtbo \
 
 dtbo-$(CONFIG_ARCH_SUN)   += oplus/ktm-camera-overlay-T0.dtbo \
+
+dtbo-$(CONFIG_ARCH_TUNA)  += tuna-camera.dtbo
+# dtbo-$(CONFIG_ARCH_TUNA)  += tuna-camera-sensor-mtp.dtbo \
+# 				tuna-camera-sensor-cdp.dtbo \
+# 				tuna-camera-sensor-qrd.dtbo
+
+dtbo-$(CONFIG_ARCH_TUNA)  += oplus/vw-camera-overlay-evb.dtbo \
+
+dtbo-$(CONFIG_ARCH_TUNA)  += oplus/vwIn-camera-overlay-evb.dtbo \
